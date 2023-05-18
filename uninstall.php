d91f065b754e82e52e5f0ac04b4d603f81404e8b
Steven Boyd <20269544+PressingButtons@users.noreply.github.com>
2023-05-18 16:17:53 -0500

    coded database clear in uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -4,4 +4,11 @@
  * @package WP Quick Events 
  */
 
- if( !defined('WP_UNINSTALL_PLUGIN')) : die; endif;
+ if( !defined('WP_UNINSTALL_PLUGIN') ) : die; endif;
+
+ //clear database stored data
+ $events = get_posts( array('post_type' => 'wp_quick_event', 'numberposts' => -1));
+
+ foreach( $events as $event ) {
+    wp_delete_post( $event->ID,  true);
+ }
